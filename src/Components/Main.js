a767edf657a609b415faf645e3e4a73d18d39618
Jonathan Ballard <asgardschosen@gmail.com>
2020-05-14 11:38:40 -0700

    finished assignment

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,7 +6,7 @@ import styles from './CSS/Main.module.css'
 class Main extends Component{
     render(){
         return (
-            <div className={styles.main}></div>
+        <div className={ styles.main }> { this.props.children  } </div>
         )
     }
 
